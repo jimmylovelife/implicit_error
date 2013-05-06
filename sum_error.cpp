b5fb30cbb773c6b30967ee08eeae3933f4662696
Sun Yongjie <sunyongjie0404@gmail.com>
2013-05-06 11:18:33 +0800

    add a error code example for strlen

--- sum_error.cpp
+++ sum_error.cpp
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * 错误代码： i <= len -1
  * len为无符号数， 当len=0时， len -1 也提升为无符号数，
  *  变成-1, 无符号数为最大值，所以i会不断循环，最后溢出array的范围
- * /
+ */
 int cal_sum(int array[], unsigned int len) {
 	int i, sum;
 	for (i = 0; i<=len-1; i++) {
@@ -12,12 +13,19 @@ int cal_sum(int array[], unsigned int len) {
 	}
 	return sum;
 }
+/**
+ * 类似错误
+ */
+int longer(char *s, char *t) {
+	return strlen(s) - strlen(t) > 0;
+}
 
 int main() {
 	int array[] = {1, 3, 4, 5};
 	unsigned int len = 0;
-	int sum = cal_sum(array, len);
+	//int sum = cal_sum(array, len);
 
-	printf("sum is %d\n", sum);
+	//printf("sum is %d\n", sum);
+	printf("longger is %d\n", longer("ab","abc"));//the result is always 1, because strlen return unsigned int
 	return 0;
 }
